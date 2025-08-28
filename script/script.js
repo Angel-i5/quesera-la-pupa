e68be8d89d9ea04c681cc480f26836f8d5348f9a
Angel-i5 <166874844+Angel-i5@users.noreply.github.com>
2025-08-28 17:43:00 -0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    if (menuToggle) {
+        menuToggle.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
+    }
+});
+
