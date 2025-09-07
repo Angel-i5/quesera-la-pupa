8bacce3fc410442dced7a2f71ba08404c17629b9
Angel-i5 <166874844+Angel-i5@users.noreply.github.com>
2025-09-07 19:15:18 -0400

    pre_entrega

--- script/script.js
+++ script/script.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
 
-    if (menuToggle) {
-        menuToggle.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
-        });
-    }
+  hamburger.addEventListener('click', function() {
+    navLinks.classList.toggle('active');
+    
+    
+  });
+   navLinks.addEventListener('click', () => {
+    navLinks.classList.remove('active');
+    
+  });
+
 });
+
 
